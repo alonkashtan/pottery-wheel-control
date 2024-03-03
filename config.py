54eaa751679a7a0d675c327d9bb3076704edd32b
alonkashtan <alonkashtan@gmail.com>
2024-03-04 00:14:23 +0200

    Fix config thresholds

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     SERVO_MIN_VALUE = const(400000)
     SERVO_MAX_VALUE = const(1330000)
 
-    PEDAL_MIN_VALUE = const(1400)
+    PEDAL_MIN_VALUE = const(4000)
     PEDAL_MAX_VALUE = const(29500)
 
     MAX_SUPPORTED_RPM = const(300)
